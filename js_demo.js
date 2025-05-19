cc3857a6281c4ef62ea580152b2b9b0daf9d412e
ElodySJTU <52987320+Elody-Chen@users.noreply.github.com>
2025-05-19 08:18:08 +0800

    Add files via upload

---
+++ js_demo.js
@@ -0,0 +1,10 @@
+// 简单交互示例
+function showMessage() {
+    document.getElementById("demo").innerHTML = "感谢你的点击！🎉";
+    alert("你好！这是一个互动示例~");
+}
+
+// 页面加载完成后执行
+window.onload = function() {
+    console.log("网站加载完成！");
+};
